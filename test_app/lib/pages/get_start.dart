7370a56d6011bb5d117cf48b23774255d622d0b6
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-08-14 15:11:20 +0800

    added with cities data and welcome

--- test_app/lib/pages/get_start.dart
+++ test_app/lib/pages/get_start.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:test_app/models/constants.dart';
+import 'package:test_app/pages/welcome.dart';
 
 class GetStart extends StatelessWidget {
   const GetStart({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Constants myConstants = Constants();
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Weather app"),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: myConstants.primaryColor.withOpacity(.5),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("assets/get-started.png"),
+              const SizedBox(height: 30,),
+              GestureDetector(
+                onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const Welcome()));
+                },
+                child: Container(
+                  height: 50,
+                  width: size.width * 0.7,
+                  decoration: BoxDecoration(
+                    color:  myConstants.primaryColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: const Center(
+                    child:  Text("Get started", style: TextStyle(color:  Colors.white, fontSize: 18),),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
