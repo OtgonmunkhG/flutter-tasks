202baa533926316cc147d81e2f4c91813a91f53c
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-08-14 14:09:04 +0800

    added : weather app structure

---
+++ test_app/lib/pages/get_start.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GetStart extends StatelessWidget {
+  const GetStart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Weather app"),
+      ),
+    );
+  }
+}
