e15387431d7d4edd5f3c3b49e89c595606a056a7
OtgonmunkhG <g.otgonmunkh@gmail.com>
2024-02-03 10:15:05 +0800

    added: lesson day 30

---
+++ lesson_day_30/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: SafeArea(child: Center(
+          child: Container(
+            
+          ),
+        )),
+      ),
+    )
+  );
+}
+
+
